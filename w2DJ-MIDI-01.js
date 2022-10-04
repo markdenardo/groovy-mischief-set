e79a93cb2474640a6b0dcb86ed721ab048e476b1
MDN <mark.denardo@gmail.com>
2022-10-03 21:34:27 -0400

    Create w2DJ-MIDI-01.js

---
+++ w2DJ-MIDI-01.js
@@ -0,0 +1,8 @@
+//
+// song 01
+const f=(()=>a.fft[0])
+const nz=(o)=>(noise([12,130,100],[88,69,00],[666,999,888]).repeat(Math.cos(0 * Math.PI / 180),1).out(o))
+const flt=(i,o,m)=>(src(i).modulateScrollY(m,f).out(o))
+nz(o0)
+flt(o0,o1,o0)
+render(o1)
